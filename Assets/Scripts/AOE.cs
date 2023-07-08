ba9747d2af203b22a69d93a7ee73c7382bcd2836
LiorGrosfeld <129890557+LiorGrosfeld@users.noreply.github.com>
2023-07-08 11:41:18 +0300

    Added Flashing AOE Effect

--- Assets/Scripts/AOE.cs
+++ Assets/Scripts/AOE.cs
@@ -5,46 +5,59 @@ using UnityEngine.UI;
 
 public class AOE : MonoBehaviour
 {
-
     public float _AOE_Flash;
     public float _AOE_Flash_Timer;
     public float _AOE_Flash_Speed;
-    
+    public bool _AOE_IsFlashing;
+    public int _AOE_FlashCounter = 0;
+    public int _AOE_Max_FlashCounter = 3;
+    public SkillsTimers _SkillsTimers_Script;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _AOE_IsFlashing = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-          if (_AOE_Flash >= _AOE_Flash_Timer)
+        if (_AOE_IsFlashing && _AOE_FlashCounter < _AOE_Max_FlashCounter)
+        {
+            if (shouldResetTimer())
             {
-               
                 // Reset timer
                 _AOE_Flash = 0f;
+                _AOE_FlashCounter++;
             }
             // Update timers
             _AOE_Flash += Time.deltaTime * _AOE_Flash_Speed;
-             // Perform Flash
+            // Perform Flash
             PerformFlash();
+        }
+        if (_AOE_FlashCounter == _AOE_Max_FlashCounter)
+        {
+            _AOE_IsFlashing = false;
+            _SkillsTimers_Script.skillsImages[0].fillAmount = 0;
+        }
+    }
+
+    private bool shouldResetTimer()
+    {
+        return _AOE_Flash >= _AOE_Flash_Timer;
     }
 
     public void PerformFlash()
     {
-
-  foreach (Transform child in transform)
+        foreach (Transform child in transform)
         {
-           // if(child.gameObject.name == "Lazer")
-          //  {
-                var image = child.GetComponent<Image>();
-                var tempColor = image.color;
-                tempColor.a = _AOE_Flash / 100; //0f;
-                image.color = tempColor;
-           // }
+            // if(child.gameObject.name == "Lazer")
+            //  {
+            var image = child.GetComponent<Image>();
+            var tempColor = image.color;
+            tempColor.a = _AOE_Flash / 100; //0f;
+            image.color = tempColor;
+            // }
         }
-        
     }
 }
--- Assets/Scripts/SkillsTimers.cs
+++ Assets/Scripts/SkillsTimers.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 public class SkillsTimers : MonoBehaviour
 {
     [SerializeField]
-    private float[] skillsTimers = new float[4];
+    private float[] skillsTimers = new float[1];
 
     [SerializeField]
-    private float[] skillsIntervals = new float[4];
+    private float[] skillsIntervals = new float[1];
 
     [SerializeField]
-    private Image[] skillsImages = new Image[4];
+    public Image[] skillsImages = new Image[1];
+    public AOE _AOE_Script;
 
     // Start is called before the first frame update
     void Start() { }
@@ -20,18 +21,26 @@ public class SkillsTimers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 1; i++)
         {
-            skillsImages[i].fillAmount = skillsTimers[i] / skillsIntervals[i];
-            if (skillsTimers[i] >= skillsIntervals[i])
+            if (skillsImages[i].fillAmount < 1)
             {
-                // Perform attack
-                PerformAttack(i);
-                // Reset timer
-                skillsTimers[i] = 0f;
+                skillsImages[i].fillAmount = skillsTimers[i] / skillsIntervals[i];
+                if (skillsTimers[i] >= skillsIntervals[i])
+                {
+                    // Perform attack
+                    PerformAttack(i);
+                    // Reset timer
+                    skillsTimers[i] = 0f;
+                }
+                // Update timers
+                skillsTimers[i] += Time.deltaTime;
             }
-            // Update timers
-            skillsTimers[i] += Time.deltaTime;
+            else
+            {
+                if (!_AOE_Script._AOE_IsFlashing)
+                    _AOE_Script._AOE_IsFlashing = true;
+            }
         }
     }
 
@@ -43,22 +52,22 @@ public class SkillsTimers : MonoBehaviour
             //  print("Laser Attack");
         }
 
-        //Wave Attack
-        if (attackIndex == 1)
-        {
-            // print("Wave Attack");
-        }
+        // //Wave Attack
+        // if (attackIndex == 1)
+        // {
+        //     // print("Wave Attack");
+        // }
 
-        //Slash Attack
-        if (attackIndex == 2)
-        {
-            //   print("Slash Attack");
-        }
+        // //Slash Attack
+        // if (attackIndex == 2)
+        // {
+        //     //   print("Slash Attack");
+        // }
 
-        //Scattar Attack
-        if (attackIndex == 3)
-        {
-            //    print("Scattar Attack");
-        }
+        // //Scattar Attack
+        // if (attackIndex == 3)
+        // {
+        //     //    print("Scattar Attack");
+        // }
     }
 }
